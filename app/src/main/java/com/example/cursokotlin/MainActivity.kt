58e2e3bf411975c02863e65b323649c88198504b
Prades <alexandre.prades.c@gmail.com>
2021-10-07 20:45:13 +0200

    35 acabado (Plantilla)

--- app/src/main/java/com/example/cursokotlin/MainActivity.kt
+++ app/src/main/java/com/example/cursokotlin/MainActivity.kt
@@ -15,50 +15,27 @@ class MainActivity : AppCompatActivity() {
 
         val textNum = findViewById<TextView>(R.id.editTextNumberDecimal)
         val button = findViewById<Button>(R.id.button)
-        val button2 = findViewById<Button>(R.id.button2)
         val result1 = findViewById<TextView>(R.id.textView)
-        val result2 = findViewById<TextView>(R.id.textView2)
-        var numerOfPieces = 0
-        var amount = 0
-        val numbers = ArrayList<Float>(0)
-
-        button.isVisible = false
+        var count1 = 0
+        var count2 = 0
 
 
-        button.setOnClickListener{
-            val num = textNum.text.toString().toFloat()
-            if (numerOfPieces == 0){
-                result1.text = "ERROR"
-            }else if(numbers.size == numerOfPieces) {
-                for (i in 0 until numbers.size) {
-                    if (numbers[i] in 1.2..1.3)
-                        amount++
-                    result1.text = "The number of the suitable parts are: $amount"
+        button.setOnClickListener {
+            val num = textNum.text.toString().toInt()
+
+            if (num > 10 || num < 0 ){
+                if (count1 == 0 && count2 == 0)
+                    result1.text ="No marks registered"
+                else{
+                    result1.text = "$count1 marks with more than 7 or 7.\n" +
+                            "$count2 marks with less than 7."
+                    button.isVisible = false
                 }
             }else{
-                numbers.add(num)
-                if (numbers.size == numerOfPieces) {
-                    button.text = "Calculate"
-                    result2.text = ""
-                }
-                else {
-                    button.text = "Length ${numbers.size + 1}"
-                    result2.text = "${numerOfPieces-numbers.size} remaining"
-                }
-            }
-        }
-
-        button2.setOnClickListener {
-            numerOfPieces = textNum.text.toString().toInt()
-            if (textNum.text == null || textNum.text == "")
-                result1.text = "Introduce a number"
-            else if (numerOfPieces == 0){
-                result1.text = "You must introduce at least one"
-            }else{
-                result2.text = "$numerOfPieces remaining"
-                button.text = "Length 1"
-                button2.isVisible = false
-                button.isVisible = true
+                if (num >= 7)
+                    count1++
+                else
+                    count2++
             }
         }
     }
