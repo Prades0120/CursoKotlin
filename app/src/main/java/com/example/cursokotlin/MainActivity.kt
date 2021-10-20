00cdde363316fabef501fa1d7206b31921dfb8d4
Prades <alexandre.prades.c@gmail.com>
2021-10-20 19:09:44 +0200

    44 acabado (Plantilla)

--- app/src/main/java/com/example/cursokotlin/MainActivity.kt
+++ app/src/main/java/com/example/cursokotlin/MainActivity.kt
@@ -16,27 +16,46 @@ class MainActivity : AppCompatActivity() {
         val textNum = findViewById<TextView>(R.id.editTextNumberDecimal)
         val button = findViewById<Button>(R.id.button)
         val result1 = findViewById<TextView>(R.id.textView)
-        var count1 = 0
-        var count2 = 0
+        var quan1 = 0
+        var quan2 = 0
+        var quan3 = 0
+        var total = 0
 
+        button.text = "Introduce"
+        textNum.hint = "Still blank or 0 to finish"
 
         button.setOnClickListener {
-            val num = textNum.text.toString().toInt()
-
-            if (num > 10 || num < 0 ){
-                if (count1 == 0 && count2 == 0)
-                    result1.text ="No marks registered"
-                else{
-                    result1.text = "$count1 marks with more than 7 or 7.\n" +
-                            "$count2 marks with less than 7."
-                    button.isVisible = false
-                }
-            }else{
-                if (num >= 7)
-                    count1++
-                else
-                    count2++
+            var num = 0.0
+            try {
+                num = textNum.text.toString().toDouble()
+            }catch (e: Exception){
             }
+
+            if (num != 0.0 ) {
+                when {
+                    num > 10.2 -> quan3++
+                    num >= 9.8 -> quan1++
+                    else -> quan2++
+                }
+                total++
+                result1.text = "Fit pieces: $quan1\n" +
+                        "Low weight pieces: $quan2\n" +
+                        "High weight pieces: $quan3\n" +
+                        "Total of pieces: $total"
+            }else{
+                if (total == 0){
+                    result1.text = "ERROR...\nNOT WEIGHTS INTRODUCED"
+                }else {
+                    button.isVisible = false
+                    textNum.isVisible = false
+                    result1.text = "FINAL RESULT:\n\n" +
+                            "Fit pieces: $quan1\n" +
+                            "Low weight pieces: $quan2\n" +
+                            "High weight pieces: $quan3\n" +
+                            "Total of pieces: $total"
+                }
+            }
+            textNum.text = ""
         }
     }
 }
