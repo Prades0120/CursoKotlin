b73e09bf16b0c14f5960f6e560bf834f0b438617
Prades <alexandre.prades.c@gmail.com>
2021-10-21 19:07:30 +0200

    47

--- app/src/main/java/com/example/cursokotlin/MainActivity.kt
+++ app/src/main/java/com/example/cursokotlin/MainActivity.kt
@@ -16,41 +16,21 @@ class MainActivity : AppCompatActivity() {
         val textNum = findViewById<TextView>(R.id.editTextNumberDecimal)
         val button = findViewById<Button>(R.id.button)
         val result1 = findViewById<TextView>(R.id.textView)
-        var total = 0
-        var account = -1
 
-        button.text = "Introduce"
-        textNum.hint = "Introduce a negative account to finish"
+        textNum.isVisible = false
+        button.text = "100"
 
 
         button.setOnClickListener {
-            var num = -1
-            try {
-                num = textNum.text.toString().toInt()
-            }catch (e: Exception){
+            var text = "Numbers: \n"
+            for (i in 1..100){
+                text += when {
+                    i == 100 -> "$i."
+                    i%10 != 0 -> "$i, "
+                    else -> "$i,\n"
+                }
             }
-
-            if (account >= 0 ) {
-                textNum.hint = "Introduce an account"
-                if (num > 0 ){
-                    result1.text = "Credit balance"
-                    total+=num
-                }else if (num == 0 )
-                    result1.text = "Null balance"
-                else
-                    result1.text = "Debit balance"
-
-                account = -1
-            }else if (account<0 && num<0){
-                button.isVisible = false
-                textNum.isVisible = false
-                result1.text = "Total of balanced credits: $total"
-            }else{
-                textNum.hint = "Introduce a balance value"
-                account = num
-                result1.text = ""
-            }
-            textNum.text = ""
+            result1.text = text
         }
     }
 }
